b2b3239a0f9e47b52162a526f37e63a5f1fab2a6
Tomaz Vieira <tom.fogaca@gmail.com>
2024-01-02 14:12:53 +0100

    Renames workspace members and crates

--- bioimg_gui/src/main.rs
+++ bioimg_gui/src/main.rs
@@ -15,7 +15,7 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Box::new(biobuilder::BioBuilderApp::new(cc))),
+        Box::new(|cc| Box::new(bioimg_gui::BioBuilderApp::new(cc))),
     )
 }
 
@@ -32,7 +32,7 @@ fn main() {
             .start(
                 "the_canvas_id", // hardcode it
                 web_options,
-                Box::new(|cc| Box::new(biobuilder::BioBuilderApp::new(cc))),
+                Box::new(|cc| Box::new(bioimg_gui::BioBuilderApp::new(cc))),
             )
             .await
             .expect("failed to start eframe");
