e658e2bfaab6fbadda0521af47aeec318abc82a3
Tomaz Vieira <tom.fogaca@gmail.com>
2024-02-02 10:52:58 +0100

    Applies rustfmt to icon_widget

--- bioimg_gui/src/widgets/icon_widget.rs
+++ bioimg_gui/src/widgets/icon_widget.rs
@@ -3,23 +3,23 @@ use bioimg_spec::rdf::icon::{Icon, IconParsingError};
 use super::StatefulWidget;
 
 #[derive(Copy, Clone, PartialEq, Eq)]
-enum InputMode{
+enum InputMode {
     Emoji,
     File,
 }
 
-pub struct StagingIcon{
+pub struct StagingIcon {
     raw: String,
     parsed: Result<Icon, IconParsingError>,
     input_mode: InputMode,
 }
 
-impl StatefulWidget for StagingIcon{
+impl StatefulWidget for StagingIcon {
     type Value<'p> = &'p Result<Icon, IconParsingError>;
 
     fn draw_and_parse(&mut self, ui: &mut egui::Ui, id: egui::Id) {
         ui.radio_value(&mut self.input_mode, InputMode::Emoji, "emoji");
-        ui.add_enabled_ui(self.input_mode == InputMode::Emoji, |ui|{
+        ui.add_enabled_ui(self.input_mode == InputMode::Emoji, |ui| {
             ui.label("Icon text (e.g. emoji): ");
             ui.text_edit_singleline(&mut self.raw);
         });
