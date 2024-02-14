bacf63a3b08ac3d658a5149d9e2c9802cdb9c9f5
Tomaz Vieira <tom.fogaca@gmail.com>
2024-02-14 19:39:49 +0100

    Adds StagingNum

--- bioimg_gui/src/widgets/mod.rs
+++ bioimg_gui/src/widgets/mod.rs
@@ -28,6 +28,43 @@ pub trait StatefulWidget {
     fn state<'p>(&'p self) -> Self::Value<'p>;
 }
 
+pub struct StagingNum<N, T> {
+    pub raw: N,
+    pub parsed: Result<T>,
+}
+
+impl<N, T> Default for StagingNum<N, T>
+where
+    N: Default,
+    T: TryFrom<N>,
+    T::Error: Display,
+{
+    fn default() -> Self {
+        Self {
+            raw: N::default(),
+            parsed: T::try_from(N::default()).map_err(|err| GuiError::new(err.to_string())),
+        }
+    }
+}
+
+impl<N, T> StatefulWidget for StagingNum<N, T>
+where
+    N: egui::emath::Numeric,
+    T: TryFrom<N> + Clone,
+    T::Error: Display + Clone,
+{
+    type Value<'p> = Result<T> where T: 'p;
+
+    fn draw_and_parse(&mut self, ui: &mut egui::Ui, id: egui::Id) {
+        ui.add(egui::widgets::DragValue::new(&mut self.raw));
+        self.parsed = T::try_from(self.raw.clone()).map_err(|err| GuiError::new(err.to_string()))
+    }
+
+    fn state<'p>(&'p self) -> Self::Value<'p> {
+        self.parsed.clone()
+    }
+}
+
 #[derive(Clone, Debug)]
 pub enum InputLines {
     SingleLine,
