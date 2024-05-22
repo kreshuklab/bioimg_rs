6c8bac73fb9c77a0b70bebbb914d7fe262e640f4
Tomaz Vieira <tom.fogaca@gmail.com>
2024-05-22 18:01:17 +0200

    Splits new GuiImage into image and texture

--- bioimg_gui/src/widgets/image_widget_2.rs
+++ bioimg_gui/src/widgets/image_widget_2.rs
@@ -1,42 +1,34 @@
 use std::{error::Error, io::Cursor, sync::Arc};
 
+use poll_promise as pp;
 use bioimg_runtime as rt;
 
 use crate::result::{GuiError, Result};
-use super::{error_display::show_error, file_source_widget::FileSourceWidgetPopupButton, staging_vec::ItemWidgetConf, util::DynamicImageExt, StatefulWidget, ValueWidget};
+use super::{error_display::show_error, file_source_widget::FileSourceWidgetPopupButton, util::DynamicImageExt, StatefulWidget, ValueWidget};
 
-pub struct GuiImage{
-    source: Option<rt::FileSource>,
-    texture_name: String,
-    image: Arc<image::DynamicImage>,
+pub type ArcDynImg = Arc<image::DynamicImage>;
+
+pub struct Texture{
+    name: String,
     context: egui::Context,
-    texture_handle: egui::TextureHandle,
+    handle: egui::TextureHandle,
 }
 
-impl GuiImage{
-    pub fn image(&self) -> Arc<image::DynamicImage>{
-        Arc::clone(&self.image)
-    }
-    pub fn load(source: rt::FileSource, context: egui::Context) -> Result<Self>{
-        let mut img_data = Vec::<u8>::new();
-        source.read_to_end(&mut img_data)?;
-        
-        let img = image::io::Reader::new(Cursor::new(img_data)).with_guessed_format()?.decode()?;
-        let texture_name: String = source.to_string();
+impl Texture{
+    pub fn load(img: &image::DynamicImage, context: egui::Context) -> Self{
+        let texture_name: String = uuid::Uuid::new_v4().to_string();
         let texture_handle = img.to_egui_texture_handle(&texture_name, &context);
-        Ok(Self{
-            source: Some(source),
-            texture_name,
-            image: Arc::new(img),
+        Self{
+            name: texture_name,
             context,
-            texture_handle,
-        })
+            handle: texture_handle,
+        }
     }
     pub fn show(&self, ui: &mut egui::Ui, display_size: egui::Vec2){
         let ui_img = egui::Image::new(
             egui::ImageSource::Texture(
                 egui::load::SizedTexture {
-                    id: self.texture_handle.id(),
+                    id: self.handle.id(),
                     size: display_size,
                 }
             )
@@ -44,9 +36,9 @@ impl GuiImage{
         ui.add(ui_img);
     }
 }
-impl Drop for GuiImage {
+impl Drop for Texture {
     fn drop(&mut self) {
-        self.context.forget_image(&self.texture_name);
+        self.context.forget_image(&self.name);
     }
 }
 
@@ -54,8 +46,8 @@ impl Drop for GuiImage {
 enum LoadingState{
     #[default]
     Empty,
-    Loading{source: rt::FileSource, promise: poll_promise::Promise<Result<GuiImage>>},
-    Ready(GuiImage),
+    Loading{source: rt::FileSource, promise: pp::Promise<Result<(ArcDynImg, Texture)>>},
+    Ready{img: ArcDynImg, texture: Texture},
     Failed(GuiError),
 }
 
@@ -91,8 +83,12 @@ impl StatefulWidget for ImageWidget2{
                         promise: {
                             let file_source = file_source.clone();
                             let context = ui.ctx().clone();
-                            poll_promise::Promise::spawn_thread(file_source.to_string(), move ||{
-                                GuiImage::load(file_source, context)
+                            pp::Promise::spawn_thread(file_source.to_string(), move ||{
+                                let mut img_data = Vec::<u8>::new();
+                                file_source.read_to_end(&mut img_data)?;
+                                let img = image::io::Reader::new(Cursor::new(img_data)).with_guessed_format()?.decode()?;
+                                let texture = Texture::load(&img, context);
+                                Ok((Arc::new(img), texture))
                             })
                         },
                         source: file_source,
@@ -103,26 +99,26 @@ impl StatefulWidget for ImageWidget2{
                     match promise.try_take(){
                         Err(promise) => LoadingState::Loading { source, promise },
                         Ok(Err(err)) => LoadingState::Failed(err),
-                        Ok(Ok(gui_img)) => LoadingState::Ready(gui_img),
+                        Ok(Ok((img, texture))) => LoadingState::Ready{img, texture},
                     }
                 },
                 LoadingState::Failed(err) => {
                     show_error(ui, &err);
                     LoadingState::Failed(err)
                 },
-                LoadingState::Ready(gui_img) => {
-                    gui_img.show(ui, egui::Vec2 { x: 50.0, y: 50.0 }); //FIXME
-                    LoadingState::Ready(gui_img)
+                LoadingState::Ready{img, texture} => {
+                    texture.show(ui, egui::Vec2 { x: 50.0, y: 50.0 }); //FIXME
+                    LoadingState::Ready{img, texture}
                 },
             }
         });
     }
 
-    fn state(&self) -> Result<Arc<image::DynamicImage>>{
+    fn state(&self) -> Result<ArcDynImg>{
         match &self.loading_state{
             LoadingState::Empty | LoadingState::Loading { .. } => Err(GuiError::new("Empty".to_owned())),
             LoadingState::Failed(err) => Err(err.clone()),
-            LoadingState::Ready(gui_img) => Ok(gui_img.image())
+            LoadingState::Ready{img, ..} => Ok(img.clone())
         }
     }
 }
