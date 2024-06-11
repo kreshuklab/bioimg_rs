67a036167cb14e5d334c05bb49a321be9e3d84e4
Tomaz Vieira <tom.fogaca@gmail.com>
2024-06-11 15:12:48 +0200

    bug: Open CollapsibleWidget on set value so it gets a chance to update next frame

--- bioimg_gui/src/widgets/collapsible_widget.rs
+++ bioimg_gui/src/widgets/collapsible_widget.rs
@@ -51,6 +51,6 @@ impl<W: ValueWidget> ValueWidget for CollapsibleWidget<W>{
 
     fn set_value<'v>(&mut self, value: Self::Value<'v>) {
         self.inner.set_value(value);
-        self.is_closed = true;
+        self.is_closed = false; //FIXME: we rely on drawing to update widgets, so we need this open =/
     }
 }
