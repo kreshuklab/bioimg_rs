a6a71374adeba4edde5b1ca13e47ae5b7c5a8673
Tomaz Vieira <tom.fogaca@gmail.com>
2024-06-11 13:56:21 +0200

    improvement: impls Display for SlashlessString

--- bioimg_spec/src/rdf/slashless_string.rs
+++ bioimg_spec/src/rdf/slashless_string.rs
@@ -1,7 +1,13 @@
-use std::{borrow::Borrow, error::Error};
+use std::{borrow::Borrow, error::Error, fmt::Display};
 
 #[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct SlashlessString<T>(T);
+
+impl<T: Display> Display for SlashlessString<T>{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
 
 impl<T: Borrow<str>> Borrow<str> for SlashlessString<T>{
     fn borrow(&self) -> &str {
