d0ce334df142f7c1edee1c33e81c581fc13deb43
Tomaz Vieira <tom.fogaca@gmail.com>
2025-02-12 13:22:34 +0000

    Adds arrow utilities

--- bioimg_gui/src/widgets/util.rs
+++ bioimg_gui/src/widgets/util.rs
@@ -79,3 +79,52 @@ impl<T> Deref for GenCell<T>{
         &self.data
     }
 }
+
+pub struct Arrow{
+    pub origin: egui::Pos2,
+    pub target: egui::Pos2,
+    pub color: egui::Color32,
+    pub tip_angle_from_shaft: f32,
+    pub tip_side_length: f32,
+}
+
+impl Arrow{
+    pub fn new(origin: egui::Pos2, target: egui::Pos2) -> Self{
+        Self{
+            origin,
+            target,
+            color: egui::Color32::BLACK,
+            tip_angle_from_shaft: std::f32::consts::PI / 9.0,
+            tip_side_length: 10.0,
+        }
+    }
+    pub fn color(mut self, color: egui::Color32) -> Self{
+        self.color = color;
+        self
+    }
+}
+
+impl Arrow{
+    pub fn draw(self, ui: &mut egui::Ui) {
+        let arrow_dir = (self.target - self.origin).normalized();
+        let reverse_arrow_dir = -arrow_dir;
+
+        let rot = egui::emath::Rot2::from_angle(self.tip_angle_from_shaft);
+        let tip_left_pt = self.target + (rot * reverse_arrow_dir * self.tip_side_length);
+        let tip_right_pt = self.target + (rot.inverse() * reverse_arrow_dir * self.tip_side_length);
+
+        let tip = egui::epaint::PathShape{
+            points: vec![self.target, tip_left_pt, tip_right_pt],
+            closed: true,
+            fill: self.color,
+            stroke: egui::Stroke{color: self.color, width: 2.0}.into(),
+        };
+
+        ui.painter().line_segment(
+            [self.origin, self.target],
+            egui::Stroke{color: self.color, width: 2.0},
+        );
+        ui.painter().add(egui::Shape::Path(tip));
+    }
+}
+
